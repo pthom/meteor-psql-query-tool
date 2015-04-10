9c1745f700c6a82cb5b1cb7918c037149880d209
Pascal Thomet <pthomet@ivsweb.com>
2015-04-10 22:52:19 +0200

    .js => .ts : Collection need to be declared as global variables

--- lib/collections.ts
+++ lib/collections.ts
@@ -1,11 +1,13 @@
 /// <reference path="../packages/typescript-libs/meteor.d.ts" />
 
 //Queries is a MongoDB collection stored in the mongo database
-var Queries = new Meteor.Collection("queries");
+declare var Queries; //:Mongo.Collection<...>;
+Queries = new Meteor.Collection("queries");
 
 
 //PsqlResults will be populated through postgresql
 //It is given no name, so that it will *not* be synchronized : cf http://docs.meteor.com/#/full/mongo_collection
 //Voir http://www.lshift.net/blog/2013/02/25/live-updates-to-meteor-from-postgres/
 //pour savoir comment avoir des notifications sur l'insert/update de donnees psql
-var PsqlResults = new Meteor.Collection("psqlresults");
+declare var PsqlResults;
+PsqlResults = new Meteor.Collection("psqlresults");
--- server/publications.ts
+++ server/publications.ts
@@ -1,4 +1,5 @@
 /// <reference path="../packages/typescript-libs/meteor.d.ts" />
+/// <reference path="../lib/collections.ts" />
 
 Meteor.publish('queries', function() {
   return Queries.find();
