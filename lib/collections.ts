dfbc122820dcde8f56cdeabb5fca8df072a82495
Pascal Thomet <pthomet@ivsweb.com>
2015-04-11 21:21:26 +0200

    Queries.allow() / dumb version for now

--- lib/collections.ts
+++ lib/collections.ts
@@ -3,3 +3,22 @@
 //Queries is a MongoDB collection stored in the mongo database
 declare var Queries; //:Mongo.Collection<...>;
 Queries = new Meteor.Collection("Queries");
+
+Queries.allow({
+  insert: function (userId, doc) {
+    //A safer alternative would be :
+    //return (userId && doc.owner === userId);
+    return true;
+  },
+  update: function (userId, doc, fields, modifier) {
+    //A safer alternative would be :
+    //return doc.owner === userId;
+    return true;
+  },
+  remove: function (userId, doc) {
+    //A safer alternative would be :
+    //return doc.owner === userId;
+    return true;
+  },
+  fetch: ['owner']
+});
