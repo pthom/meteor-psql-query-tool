b6610b57728c4a0cba817be7413aec8b206a1852
Pascal Thomet <pthomet@ivsweb.com>
2015-04-11 14:56:17 +0200

    Code cleaning : much less code with webix...

--- client/views/home.js
+++ client/views/home.js
@@ -2,33 +2,10 @@ Template.home.helpers({
   queries: function () {
       return Queries.find({}, {});
   },
-  selected_query: function() {
-      return Queries.findOne( Session.get("currentQueryId") );
-  },
   QueryResultRows: function() {
       return ServerSession.get("QueryResult").rows;
   }
 });
 
 Template.home.events({
-    "click #btn_save_query" : function(event) {
-        var name = $("#txt_name").val();
-        var query = $("#txt_query").val();
-        Meteor.call("saveQuery", this._id, name, query);
-    },
-    "click #btn_new_query" : function(event) {
-        Meteor.call("addQuery");
-    },
-    "click #btn_delete_query" : function(event) {
-        console.log("about to delete");
-        Meteor.call("deleteQuery", this._id);
-    },
-    "click #btn_select_query" : function(event) {
-        Session.set("currentQueryId", this._id);
-    },
-    "click #btn_run_query" : function(event) {
-        console.log("about to run");
-        Meteor.call("runPostgresqlQuery", this.query);
-    },
-
 });
--- server/server.ts
+++ server/server.ts
@@ -6,25 +6,6 @@ console.log("Load server.js");
 declare var ServerSession;
 
 Meteor.methods({
-    addQuery: function() {
-        Queries.insert({
-            name: "New Query",
-            query: "",
-        });
-    },
-    deleteQuery: function(id) {
-        Queries.remove(id);
-    },
-    saveQuery: function(id, name, query) {
-        Queries.update(id, {
-            $set: {
-                name: name,
-                query: query
-            }
-        });
-    },
-
-    //this will update the Meteor|Mongo.Collection PsqlResult
     runPostgresqlQuery: function(sql_query) {
         console.log("runPostgresqlQuery : " + sql_query);
         //pg_query is async, but pg_query_wrapAsync is sync,
