8118f2d154aae440c4a3586199ee55c2798aefc1
Pascal Thomet <pthomet@ivsweb.com>
2015-04-11 14:56:40 +0200

    Displa query result (still need to be synced)

--- client/client.js
+++ client/client.js
@@ -1,5 +1,24 @@
 //declare var webix;
 
+
+function PutSqlDataIntoTable(data, table)
+{
+  //debugger;
+  table.clearAll();
+
+  columnConfig = [];
+
+  for (idx = 0; idx < data.fields.length; ++idx) {
+    var field = data.fields[idx];
+    var colname = field.name;
+    columnConfig.push({id:colname, header:colname, sort:"string", adjust:"data"});
+  }
+
+  table.refreshColumns(columnConfig);
+  table.parse(data.rows);
+
+}
+
 function MakeWebixUi_Queries() {
   var querytable = {
     //container:'webix_queries_div',
@@ -55,13 +74,28 @@ function MakeWebixUi_Queries() {
       name: 'query',
       label: 'Query SQL'
     }, {
-      view: 'button',
-      label: 'Save',
-      type: 'form', // a Submit button; 'form' is an odd type name for buttons - http://docs.webix.com/api__ui.button_type_config.html#comment-1863007844
-      click: function() {
-        this.getFormView().save();
-        this.getFormView().clear();
-      }
+      view: 'layout',
+      cols: [{
+          view: 'button',
+          label: 'Run',
+          click: function() {
+            var sql = $$('querytable').getSelectedItem().query;
+            //alert("Run : " + sql);
+            Meteor.call("runPostgresqlQuery", sql);
+            var data = ServerSession.get("QueryResult");
+            PutSqlDataIntoTable(data, $$('queryresulttable'));
+          },
+        },
+        {
+          view: 'button',
+          label: 'Save',
+          type: 'form', // a Submit button; 'form' is an odd type name for buttons - http://docs.webix.com/api__ui.button_type_config.html#comment-1863007844
+          click: function() {
+            this.getFormView().save();
+            this.getFormView().clear();
+          },
+        },
+      ]
     }]
   };
 
@@ -73,6 +107,12 @@ function MakeWebixUi_Queries() {
   };
 
   var webixContainer = webix.ui(panel);
+  // The problem with mixing Webix components and non-Webix HTML markup is that Webix UI components won't resize
+  // automatically if you place them in an HTML container. You have to resize them manually, like below.
+  // Read more at http://forum.webix.com/discussion/comment/3650/#Comment_3650.
+  webix.event(window, 'resize', function() {
+    webixContainer.resize();
+  });
 
   // http://docs.webix.com/desktop__data_binding.html
   $$('queryForm').bind($$('querytable'));
@@ -80,17 +120,33 @@ function MakeWebixUi_Queries() {
 }
 
 
-
-Meteor.startup(function() {
-  MakeWebixUi_Queries();
-
-
+function MakeWebixUi_QueryResult() {
+  var queryresulttable = {
+    container: 'webix_queryresult_div',
+    view: 'datatable',
+    id: 'queryresulttable',
+    autoConfig: true, // infer columns from data
+    select: true,
+    sortable: true,
+    editable: true,
+    resizeColumn: true,
+    data: [ {content : "goes", here:"yes"} ]
+  };
+  var webixContainer = webix.ui(queryresulttable);
   // The problem with mixing Webix components and non-Webix HTML markup is that Webix UI components won't resize
   // automatically if you place them in an HTML container. You have to resize them manually, like below.
   // Read more at http://forum.webix.com/discussion/comment/3650/#Comment_3650.
   webix.event(window, 'resize', function() {
     webixContainer.resize();
   });
+}
+
+
+Meteor.startup(function() {
+  MakeWebixUi_Queries();
+  MakeWebixUi_QueryResult();
+
+
 
 
 });
