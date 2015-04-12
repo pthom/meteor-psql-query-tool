9e9b4ad7804b26e632eb6629a29e597922aa4674
Pascal Thomet <pthomet@ivsweb.com>
2015-04-12 03:39:50 +0200

    Code cleanup

--- client/views/1_query_result_view.js
+++ client/views/1_query_result_view.js
@@ -6,45 +6,6 @@
 query_result_view = {
 
   ui_definition : function() {
-/*
-    var queryList = {
-      view: 'form',
-      id: 'queryList',
-      elements: [{
-        view: 'text',
-        name: 'name',
-        label: 'Query Name'
-      }, {
-        view: 'text',
-        name: 'query',
-        label: 'Query SQL'
-      }, {
-        view: 'layout',
-        cols: [{
-            view: 'button',
-            label: 'Run',
-            click: function() {
-              var sql = $$('querytable').getSelectedItem().query;
-              //alert("Run : " + sql);
-              Meteor.call("runPostgresqlQuery", sql);
-              var data = ServerSession.get("QueryResult");
-              populate_datatable_psql_results(data, $$('queryresulttable'));
-            },
-          },
-          {
-            view: 'button',
-            label: 'Save',
-            type: 'form', // a Submit button; 'form' is an odd type name for buttons - http://docs.webix.com/api__ui.button_type_config.html#comment-1863007844
-            click: function() {
-              this.getFormView().save();
-              this.getFormView().clear();
-            },
-          },
-        ]
-      }]
-    };
-*/
-
 
     var queryresulttable = {
       view: 'datatable',
