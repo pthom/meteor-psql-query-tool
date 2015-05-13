0f1bf828c26183491e1baf75257ac42f3e7a45f6
Pascal Thomet <pthomet@ivsweb.com>
2015-05-13 22:57:51 +0200

    MainUi : destroy / recreate view properly

--- client/views/MainUi.js
+++ client/views/MainUi.js
@@ -1,21 +1,46 @@
 
+var webixMainUi = null;
+var webixQueryEditWindow = null;
+var webix_json_export_view = null;
+
+function DestroyWebixView(view) {
+  for (var i = 0; i < view.getChildViews().length; i++) {
+    DestroyWebixView(view.getChildViews[i]);
+    view.destructor();
+  }
+}
+
+function CleanPreviousUi() {
+  if (webixMainUi) {
+    DestroyWebixView(webixMainUi);
+    webixMainUi = null;
+  }
+  if (webixQueryEditWindow) {
+    DestroyWebixView(webixQueryEditWindow);
+    webixQueryEditWindow = null;
+  }
+  if (webix_json_export_view) {
+    DestroyWebixView(webix_json_export_view);
+    webix_json_export_view = null;
+  }
+}
+
 
 MainUi = function() {
-  //if ( ! Meteor.userId())
-    //return;
+  //debugger;
+  CleanPreviousUi();
 
   var ui_def = main_view.ui_definition();
-  var webixContainer = webix.ui(ui_def);
+  webixMainUi = webix.ui(ui_def, webixMainUi);
 
   // The problem with mixing Webix components and non-Webix HTML markup is that Webix UI components won't resize
   // automatically if you place them in an HTML container. You have to resize them manually, like below.
   // Read more at http://forum.webix.com/discussion/comment/3650/#Comment_3650.
   webix.event(window, 'resize', function() {
-    webixContainer.resize();
+    webixMainUi.resize();
   });
-
-  webix.ui(query_edit_view.ui_definition());
-  webix.ui(json_export_view.ui_definition());
+  webixQueryEditWindow = webix.ui(query_edit_view.ui_definition());
+  webix_json_export_view = webix.ui(json_export_view.ui_definition());
   query_list_view.do_bind();
   main_view.on_document_ready();
 
